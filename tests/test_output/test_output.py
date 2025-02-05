d2d798592bc3c63db3a044b59b25a8d78735a741
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2025-02-05 16:12:53 +0000

    Update for latest beautifulsoup.

--- tests/test_output/test_output.py
+++ tests/test_output/test_output.py
@@ -3,12 +3,13 @@ import random
 
 # 3rd party
 import pytest
-from bs4 import BeautifulSoup  # type: ignore
+from bs4 import BeautifulSoup
 from domdf_python_tools.paths import PathPlus
 from pytest_regressions.file_regression import FileRegressionFixture
+from sphinx.application import Sphinx
 
 
-def test_build_example(app):
+def test_build_example(app: Sphinx):
 	app.build()
 	app.build()
 
@@ -20,11 +21,11 @@ def test_html_output(page: BeautifulSoup, file_regression: FileRegressionFixture
 
 
 @pytest.mark.sphinx("latex", srcdir="test-root")
-def test_latex_output(app):
+def test_latex_output(app: Sphinx):
 	random.seed("5678")
 
 	assert app.builder.name.lower() == "latex"
 	app.build()
 
-	assert not PathPlus(app.outdir / "_debug" / "index.html").is_file()
-	assert not PathPlus(app.outdir / "_debug").is_dir()
+	assert not PathPlus(app.outdir / "_debug" / "index.html").is_file()  # type: ignore[operator]
+	assert not PathPlus(app.outdir / "_debug").is_dir()  # type: ignore[operator]
